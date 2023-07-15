ef81da649252248a3b4473283fb33a0ac0b25846
NataliaMazurak <mazurak25@gmail.com>
2023-07-15 21:11:32 +0300

    08 homework

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,50 @@
 
+
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+
+
+// timeUpdate зберігає поточний час в localStorage 
+
+function timeUpdate(event) {
+    const currentTime = event.seconds;
+    // console.log(currentTime);
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentTime));
+}
+
+
+player.on('timeupdate', throttle(timeUpdate, 1000));
+
+function timeUpdate(event) {
+    const currentTime = event.seconds;
+    console.log(currentTime);
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(currentTime));
+}
+
+// saveCurrentTime - останній  збережений час або за замовчуванням 0
+
+const savedCurrentTime = localStorage.getItem(LOCALSTORAGE_KEY) || 0;
+
+
+// setCurrentTime () - встановлює відео на позицію,яку мизберегли в localStorage, також перевиряємо тип помилки
+player
+  .setCurrentTime(savedCurrentTime)
+  .then(function (seconds) {
+    console.log(`saveCurrentTime is ${seconds} seconds`);
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        console.log('Current Time error');
+        break;
+
+      default:
+        console.log('This is default');
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,47 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+feedbackForm.addEventListener('submit', onFormSubmit);
+feedbackForm.addEventListener('input', throttle(handleSubmit, 500));
+
+populateForm();
+
+// onFormSubmit - очищає поля форми та видаляє дані з localStorage
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+// handleSubmit - отримуємо та оновлюємо дані форми в localStorage
+
+function handleSubmit(event) {
+  const savedForm = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
+  savedForm[event.target.name] = event.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(savedForm));
+
+}
+
+// populateForm() - заповняємо поля форми значеннями що збережені в localStorage
+
+function populateForm(event) {
+    const savedForm = localStorage.getItem(LOCALSTORAGE_KEY);
+//   console.log(savedForm)
+
+    if (savedForm) {
+        
+      const formFields = JSON.parse(savedForm);
+      emailInput.value = formFields.email
+      messageInput.value = formFields.message
+  
+  
+    }
+  
+  }
